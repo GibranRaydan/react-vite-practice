9af8ae5549a590a5a0a852b1434ac335c99880ad
unknown <superpegorino@gmail.com>
2023-08-31 12:03:07 -0500

    initial

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,36 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+import { Navbar } from '../../Components/Navbar';
+import Home from '../Home';
+import { ShoppingCartProvider } from '../../Context';
+import MyAccount from '../MyAccount';
+import MyOrder from '../MyOrder';
+import MyOrders from '../MyOrders';
+import NotFound from '../NotFound';
+import Signin from '../Signin';
+import './App.css';
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/sign-in', element: <Signin /> },
+    { path: '/*', element: <NotFound /> }
+  ])
+
+  return routes
+}
+
+const App = () => {
+  return (
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <AppRoutes />
+        <Navbar />
+      </BrowserRouter>
+    </ShoppingCartProvider>
+  )
+}
+
+export default App
---
+++ src/Pages/Signin/index.jsx
@@ -0,0 +1,12 @@
+import { Layout } from "../../Components/Layout"
+
+function Signin() {
+
+  return (
+    <Layout>
+      Sign In
+    </Layout>
+  )
+}
+
+export default Signin
